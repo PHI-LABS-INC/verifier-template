7f08be8b8671c16d3bebbd04e48f713e2e7bed61
ZaK3939 <tsu.aria.kalei@gmail.com>
2024-07-20 17:27:06 +0400

    feat: update template

--- api/verify-basic.ts
+++ api/verify-basic.ts
@@ -10,7 +10,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
   }
 
   const [mint_eligibility, _] = await check_cred(address as string, 0);
-  console.log(`Cred check result: ${mint_eligibility});
+  console.log(`Cred check result: ${mint_eligibility}`);
 
   const signature = await create_signature(address as Address, mint_eligibility, '0');
   return res.status(200).json({ mint_eligibility, signature });
